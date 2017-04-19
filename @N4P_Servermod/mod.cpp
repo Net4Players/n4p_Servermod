7e6923b5bd6584d55ab36780acdf22543812d96e
XylofonKobra414 <xylofonkobra414@gmail.com>
2017-04-19 14:10:50 +0200

    First Upload
    First upload with the newest Modfiles

---
+++ @N4P_Servermod/mod.cpp
@@ -0,0 +1,15 @@
+name = "Net4Players Servermod";
+picture = "net4players_servermod_logo.paa";
+actionName = "";
+action = "";
+description = "This addon adds a few specific things for our Server 'Willkommen im Dreck #1'";
+logo = "net4players_servermod_logo.paa";
+logoOver = "net4players_servermod_logo.paa";
+tooltip = "Net4Players Servermod";
+tooltipOwned = "Net4Players Servermod Owned";
+overview = "This addon adds a few specific things for our Server 'Willkommen im Dreck #1'";
+author = "XylofonKobra414, Darkwulf, Old Aeneas";
+overviewPicture = "net4players_servermod_logo.paa";
+overviewText = "";
+overviewFootnote = "";
+
